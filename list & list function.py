4d8ca5ddcf0ee3959322b9c2cb4c81f4274ab893
katkarrushikesh9356-del <katkarrushikesh9356@gmail.com>
2025-12-04 19:51:41 +0530

    Add files via upload

---
+++ list & list function.py
@@ -0,0 +1,15 @@
+grocery = ["Harpic","vim bar","bhindi","lollypop",34]
+print(grocery[3])
+number =[2,4,6,5,4]
+number.sort()
+number. reverse()
+number.pop()
+number.remove(4)
+number.append(7)
+number.append(72)
+number = []
+print(number[1:3])
+print(number[1:5:2])
+number. insert(1,65)
+print(number)
+ 
